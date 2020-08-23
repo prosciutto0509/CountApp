1879333ab4c6e2b4fa442c031c67c3213e28f9ac
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-08-23 14:50:22 +0900

    Initial Commit

---
+++ CountApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountApp
+//
+//  Created by 安田 悠麿 on 2020/08/23.
+//  Copyright © 2020 安田 悠麿. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
